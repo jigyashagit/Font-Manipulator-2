d30e172a447e556bc1d48d3c3410e7dff52333d7
jigyashagit <109466868+jigyashagit@users.noreply.github.com>
2023-01-20 20:34:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function setup()
+{
+    video=createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas=createCanvas(550,500);
+    canvas.position(560,150);
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+
+function draw()
+{
+    background('pink');
+}
+function modelLoaded()
+{
+    console.log('PoseNet is Intialized');
+}
+function gotPoses(results){
+    if(results.length>0)
+    {
+        console.log(results);
+    }
+    
+}
